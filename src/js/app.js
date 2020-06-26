b75d49dc8e6d661d792e0a511c193efe2c41e1bf
Danlois R Tovar T <danlois.tovar@gmail.com>
2020-06-26 00:46:15 -0300

    se agrego CDN bootstrap y fontawesome al index html

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,9 @@ app.set('port', 3000);
 // agregar motor de plantillas:
 app.set('view engine', 'ejs');
 
+//configurar extenciones para el motor de plantillas:
+app.engine('html', require('ejs').renderFile);
+
 //indicar rutas de carpetas (cuando no son localizadas):
 // --> app setea y buscame el archivo views en la raiz y en la carpeta views:
 app.set('views', path.join(__dirname, '../views'));
--- src/routers/router.js
+++ src/routers/router.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 // Ruta pagina inicial:
 router.get('/', (req, res) => {
-  res.render('index', { title: 'web 2 desde node' });
+  res.render('index.html', { title: 'web 2 desde node' });
 });
 
 //------------------------------------------
 
 // Ruta pagina contacto:
 router.get('/contacto', (req, res) => {
-  res.render('contacto', { title: 'web 2 desde node' });
+  res.render('contacto.html', { title: 'web 2 desde node' });
 });
 
 module.exports = router;
