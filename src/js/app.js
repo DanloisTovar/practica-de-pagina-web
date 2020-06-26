64de238dacd62aff75db795b629d61342c2bc274
Danlois R Tovar T <danlois.tovar@gmail.com>
2020-06-25 22:49:15 -0300

    se agrego la estructura y script inicial del servidor node

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,22 @@
-console.log('servidor funcionando');
+const express = require('express');
+const app = express();
+//----------------------------------
+
+//configuraciones:
+//con esto se crea la constante que define al numero de puerto.
+app.set('port', 3000);
+//--------------------------------------------------------------
+
+// Middlewares:
+
+//--------------------------------------------------------------
+
+// Rutas:
+//--------------------------------------------------------------
+
+// static folder (carpetas estaticas):
+//--------------------------------------------------------------
+//Puerto escuchando:
+app.listen(app.get('port'), () => {
+  console.log('servidor escuchando por el puerto:', app.get('port'));
+});
